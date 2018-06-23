3c57dbba0e5e38f4f623e537f8dabe0fe06f3a63
jonpecson <jonpecson@gmail.com>
2018-06-23 21:34:39 +0800

    feat(Auth provider): Handles register, login and removal of the logged in account.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { HomePage } from '../pages/home/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SecureStorage } from '@ionic-native/secure-storage';
 
 import { App } from '../providers/app/app';
 import { ComponentsModule } from './../components/components.module';
+import { AuthProvider } from '../providers/auth/auth';
 
 @NgModule({
   declarations: [
@@ -34,8 +36,10 @@ import { ComponentsModule } from './../components/components.module';
   providers: [
     StatusBar,
     SplashScreen,
+    SecureStorage,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    App
+    App,
+    AuthProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/auth/auth.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { SecureStorage } from '@ionic-native/secure-storage';
+
+/*
+  Generated class for the AuthProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class AuthProvider {
+  constructor(private secureStorage: SecureStorage) {
+    console.log('Hello AuthProvider Provider');
+  }
+
+  /**
+   * Login the user.
+   * @param email { string } The email of the user
+   * @param password { string } The password of the user
+   */
+  login(email: string, password: string) {
+    return this.secureStorage.create('auth').then(storage => {
+      return Promise.all([storage.get('email'), storage.get('password')]).then(
+        data => {
+          const loggedInAccount = {
+            email: data[0],
+            password: data[1]
+          };
+
+          if (
+            // Incorrect email and correct password
+            (loggedInAccount.email !== email &&
+              loggedInAccount.password === password) ||
+            // Incorrect password and correct email
+            (loggedInAccount.email === email &&
+              loggedInAccount.password !== password) ||
+            // Incorrect password and incorrect email
+            (loggedInAccount.email !== email && loggedInAccount.password !== password)
+          ) {
+            return {
+              status: 'failed',
+              message:
+                "It looks like there's something wrong with your username of password you entered. Please try again."
+            };
+          }
+
+          return {
+            status: 'success'
+          };
+        }
+      );
+    });
+  }
+
+  /**
+   * Register new user.
+   * @param email { string } The email of the user
+   * @param password { string } The password of the user
+   */
+  register(email: string, password: string) {
+    return this.secureStorage.create('auth').then(storage => {
+      return Promise.all([
+        storage.set('email', email),
+        storage.set('password', password)
+      ]);
+    });
+  }
+
+  /**
+   * Removes the email and password of the logged in user.
+   */
+  remove() {
+    return this.secureStorage.create('auth').then(storage => {
+      return Promise.all([
+        storage.remove('email'),
+        storage.remove('password')
+      ]);
+    });
+  }
+}
