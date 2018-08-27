099c6cb0d5e28ea5dba7dd4a6cda694786d1b9ef
Jon Pecson <jonpecson.io@gmail.com>
2018-08-27 10:48:38 +0800

    Update resources

--- src/providers/nem/nem.ts
+++ src/providers/nem/nem.ts
@@ -51,7 +51,7 @@ export class NemProvider {
   accountOwnedMosaicsService: AccountOwnedMosaicsService;
 
   constructor() {
-    NEMLibrary.bootstrap(NetworkTypes.MAIN_NET);
+    NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
 
     if (NEMLibrary.getNetworkType() === NetworkTypes.TEST_NET) {
       this.accountHttp = new AccountHttp(SERVER_CONFIG);
