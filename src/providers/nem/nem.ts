5b59b2afe00b02aaab735c873387c8f39cefbd86
Jon Pecson <jonpecson.io@gmail.com>
2018-08-29 22:11:05 +0800

    chore(Update logo, icon and network type to MAIN_NET): Change network type to MAIN_NET in nem.ts

--- src/providers/nem/nem.ts
+++ src/providers/nem/nem.ts
@@ -59,7 +59,7 @@ export class NemProvider {
   accountOwnedMosaicsService: AccountOwnedMosaicsService;
 
   constructor() {
-    NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
+    NEMLibrary.bootstrap(NetworkTypes.MAIN_NET);
 
     if (NEMLibrary.getNetworkType() === NetworkTypes.TEST_NET) {
       this.accountHttp = new AccountHttp(SERVER_CONFIG);
