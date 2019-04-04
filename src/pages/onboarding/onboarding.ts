b0163473c111948962888eafa582e211c79f9226
John Pecson <jonpecson.io@gmail.com>
2019-04-04 14:21:47 +0800

    refactor: update wallet guide

--- src/pages/onboarding/onboarding.ts
+++ src/pages/onboarding/onboarding.ts
@@ -47,8 +47,7 @@ export class OnboardingPage {
       {
         title: 'What is ProximaX Wallet?',
         description:
-          `The ProximaX Wallet app is a free, client-side interface which allows you to interact directly with
-          the blockchain while you remain in full control of your keys and funds.
+          `The ProximaX Wallet app is a free, client-side interface which allows you to interact directly with the blockchain while you remain in full control of your keys and funds. 
           <br><br>
           At ProximaX, we are committed to helping you secure your tokens and all our wallet updates
 undergo vigorous testing in order to making sure that everything you hold in your wallet is safe.
@@ -59,13 +58,16 @@ However, a large part of keeping your wallet and its contents safe relies on YOU
       {
         title: 'What if I lost my keys or password?',
         description:
-          `When you first launch the ProximaX Wallet app, it creates a private key for you. A private key is a
-          string of alpha-numeric characters that allows you to access the tokens you hold that are stored on
-          the blockchain via the ProximaX Wallet.
+          `When your private key is generated, it is known to only you. We have absolutely no access to it. We can’t access accounts, recover keys, reset passwords or reverse transactions for you. 
           <br><br>
-          The role of the private key is to give the holder of the key unrestricted access to the tokens it
-represents on this wallet. This private key can be used to restore the contents of your wallet in the
-event that your phone is stolen, lost or does not work anymore.
+          <strong>If you lose your private keys, it is GONE FOREVER. </strong>
+          <br> <br>
+          Once you have received your keys and set up your password, we strongly suggest you:
+          <br>
+          <ul>
+            <li>Write your keys or password down. Keep them safe.</li><br>
+            <li>Do not store your keys and password on your computer or you phone.ea</li>
+          </ul>.
           `,
         isSmall: true,
         image: App.SLIDES.SLIDE2
@@ -73,47 +75,47 @@ event that your phone is stolen, lost or does not work anymore.
       {
         title: 'Where are my funds stored?',
         description:
-          `All tokens actually reside on the blockchain itself and the wallet is merely a doorway that allows you to
-          interact with the blockchain in a convenient way. Therefore, we do not control your tokens in any
-          way.
+          `Contrary to popular belief, your tokens are never stored in a crypto wallet. All tokens  are on the blockchain itself and we do not control your tokens in any way.
+          <br><br>
+          <strong>Helpful Tips</strong>
           <br>
           <ul>
-            <li>Write your keys or password down. Keep them safe.</li>
-            <li>Do not store your keys and password on your computer or you phone.ea</li>
+            <li>Never give your private keys to anyone. </li><br>
+            <li>We will never ask you for your private key (and neither should anyone).
+            </li><br>
+            <li>We don’t give away free XPX.</li><br>
+            <li>We will never ask you to send us XPX.</li>
           </ul>`,
         isSmall: true,
         image: App.SLIDES.SLIDE3
       },
       {
-        title: 'Some Helpful Tips',
+        title: 'Why use cryptocurrency and blockchain?',
         description:
-          `<ul>
-            <li><strong>Never give your private keys to anyone.</strong> <br>
-            That includes your keystore file and mnemonic phrase.</li>
-            <li><strong>Don’t trust any free XPX.</strong>  <br>
-            If they’re giving away free XPX, it’s not real. If they email you saying they’re us, it’s not real. If they ask for your private key, it’s not real.</li>
-            <li><strong>Feel free to contact us.</strong>  <br>
-            Please feel free to contact us at info@proximax.io or join our official telegram group @ProximaXio</li>
-          </ul>
+          `The purpose of cryptocurrency is to allow people to manage their funds in an anonymous and secure way, from any location without relying on third parties.
+          <br><br>
+          On the blockchain, your funds are not controlled by any bank or any party. You are not bound to any authorizations, permissions or limits.
+          <br><br>
+          Your public address and your private keys are the only pieces of information you need to hold to manage your funds from anywhere in the world.
           `,
         isSmall: true,
         image: App.SLIDES.SLIDE4
       },
-      {
-        title: 'What’s the upside?',
-        description:
-          `The purpose of cryptocurrency is to allow people to manage their funds in an anonymous and secure way, from any location without relying on third parties.
+      // {
+      //   title: 'What’s the upside?',
+      //   description:
+      //     `The purpose of cryptocurrency is to allow people to manage their funds in an anonymous and secure way, from any location without relying on third parties.
 
-          <br> <br>
+      //     <br> <br>
 
-          On the blockchain, your funds are not controlled by any bank. You are the only one who has access to your funds and you can instantly transfer to any other address on the blockchain without depending on any authorizations, permissions or limits.
+      //     On the blockchain, your funds are not controlled by any bank. You are the only one who has access to your funds and you can instantly transfer to any other address on the blockchain without depending on any authorizations, permissions or limits.
           
-          <br> <br>
+      //     <br> <br>
 
-          Your public address and your private keys are the only pieces of information you need to hold to manage your funds from anywhere in the world.`,
-        isSmall: true,
-        image: App.SLIDES.SLIDE5
-      }
+      //     Your public address and your private keys are the only pieces of information you need to hold to manage your funds from anywhere in the world.`,
+      //   isSmall: true,
+      //   image: App.SLIDES.SLIDE5
+      // }
     ];
   }
 
