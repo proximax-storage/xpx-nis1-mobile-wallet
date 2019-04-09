b613885b3b0456e8b63345f6ae25958fdcee7229
John Pecson <jonpecson.io@gmail.com>
2019-04-09 23:01:39 +0800

    refactor: activate telegram link in the onboarding screen

--- src/pages/onboarding/onboarding.ts
+++ src/pages/onboarding/onboarding.ts
@@ -47,56 +47,48 @@ export class OnboardingPage {
       {
         title: 'What is ProximaX Wallet?',
         description:
-          `The ProximaX Wallet app is a free, client-side interface which allows you to interact directly with the blockchain while you remain in full control of your keys and funds. 
-          <br><br>
-          At ProximaX, we are committed to helping you secure your tokens and all our wallet updates
-undergo vigorous testing in order to making sure that everything you hold in your wallet is safe.
-However, a large part of keeping your wallet and its contents safe relies on YOU.`,
+          `<br>&nbsp;<br>
+          At ProximaX, we are committed to helping you secure your digital assets. All our wallet updates undergo rigorous testing in order to ensure that everything you hold in your wallet is safe. However, a large part of keeping your wallet  and its contents safe depends on YOU.`,
         isSmall: true,
         image: App.SLIDES.SLIDE1
       },
       {
-        title: 'What if I lost my keys or password?',
+        title: 'Securing Your Private Keys and Password',
         description:
-          `When your private key is generated, it is known to only you. We have absolutely no access to it. We can’t access accounts, recover keys, reset passwords or reverse transactions for you. 
-          <br><br>
-          <strong>If you lose your private keys, it is GONE FOREVER. </strong>
-          <br> <br>
-          Once you have received your keys and set up your password, we strongly suggest you:
+          `<br>&nbsp;<br><strong>We strongly recommend you to:</strong>
           <br>
+          
           <ul>
-            <li>Write your keys or password down. Keep them safe.</li><br>
-            <li>Do not store your keys and password on your computer or you phone.ea</li>
+            <li>Write down your private keys and password and store them in a safe place.</li><br>
+            <li>Do not store your keys and password on your computer or phone especially if they are assessible by other parties.</li>
           </ul>.
           `,
         isSmall: true,
-        image: App.SLIDES.SLIDE2
+        image: App.SLIDES.SLIDE3
       },
       {
-        title: 'Where are my funds stored?',
+        title: 'Where are my digital tokens stored?',
         description:
-          `Contrary to popular belief, your tokens are never stored in a crypto wallet. All tokens  are on the blockchain itself and we do not control your tokens in any way.
+          `Contrary to popular belief, your digital assets are never stored in a crypto wallet. All digital assets reside on the blockchain itself and ProximaX does not control your digital assets in any way.
           <br><br>
-          <strong>Helpful Tips</strong>
+          <strong "padding-left: 10px;">IMPORTANT!</strong>
           <br>
           <ul>
-            <li>Never give your private keys to anyone. </li><br>
-            <li>We will never ask you for your private key (and neither should anyone).
+            <li>Never reveal your private keys to anyone. </li><br>
+            <li>We will never ask you for your private keys (and neither should anyone).
             </li><br>
             <li>We don’t give away free XPX.</li><br>
             <li>We will never ask you to send us XPX.</li>
           </ul>`,
         isSmall: true,
-        image: App.SLIDES.SLIDE3
+        image: App.SLIDES.SLIDE2
       },
       {
         title: 'Why use cryptocurrency and blockchain?',
         description:
-          `The purpose of cryptocurrency is to allow people to manage their funds in an anonymous and secure way, from any location without relying on third parties.
+          `The purpose of cryptocurrency is to allow you to take full control and manage your digital tokens in an anonymous and secure way, from any location without relying on third parties.
           <br><br>
-          On the blockchain, your funds are not controlled by any bank or any party. You are not bound to any authorizations, permissions or limits.
-          <br><br>
-          Your public address and your private keys are the only pieces of information you need to hold to manage your funds from anywhere in the world.
+          The only information needed to do this is to have your public address and private keys. 
           `,
         isSmall: true,
         image: App.SLIDES.SLIDE4
