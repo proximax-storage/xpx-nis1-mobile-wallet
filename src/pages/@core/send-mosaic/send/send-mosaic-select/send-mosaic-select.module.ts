458974f73c9d76383730675b67c79408f45d0a33
John Pecson <jonpecson.io@gmail.com>
2019-04-12 02:14:32 +0800

    fix:
    fix issue on total amount computation in usd on  mosaic selection pop-up

--- src/pages/@core/send-mosaic/send/send-mosaic-select/send-mosaic-select.module.ts
+++ src/pages/@core/send-mosaic/send/send-mosaic-select/send-mosaic-select.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SendMosaicSelectPage } from './send-mosaic-select';
+import { PipesModule } from '../../../../../pipes/pipes.module';
 
 @NgModule({
   declarations: [
     SendMosaicSelectPage,
   ],
   imports: [
-    IonicPageModule.forChild(SendMosaicSelectPage),
+    IonicPageModule.forChild(SendMosaicSelectPage), PipesModule
   ],
 })
 export class SendMosaicSelectPageModule {}
