17c9e97668a8fb32bf55a88ad4d2c728321aee2a
John Pecson <jonpecson.io@gmail.com>
2019-05-08 09:57:27 +0800

    refactor: fix 14D graph issue

--- src/pages/@core/coin-price-chart/coin-price-chart.ts
+++ src/pages/@core/coin-price-chart/coin-price-chart.ts
@@ -96,7 +96,7 @@ export class CoinPriceChartPage {
     this.durations = [
       { label: "24H", value: 1 },
       { label: "7D", value: 7 },
-      { label: "14D", value: 365 },
+      { label: "14D", value: 14 },
       { label: "30D", value: 30 },
       { label: "6M", value: 182 }
 
